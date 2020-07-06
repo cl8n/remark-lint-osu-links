0d681945e3e377e518b7704190ef41a9ae64f395
clayton <me@clayton.cc>
2020-07-06 01:02:58 -0700

    Use stricter matching for URIs

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ var rule = require('unified-lint-rule');
 var generated = require('unist-util-generated');
 var visit = require('unist-util-visit');
 
-var uriRegex = /https?:\/\/(?:osu|new)\.ppy\.sh\/([^\/]+)/;
-var uriWarnings = {
-    b: 'Beatmap URLs should use the /beatmapsets/{beatmapset-id}#{mode}/{beatmap-id} format',
-    beatmaps: 'Beatmap URLs should use the /beatmapsets/{beatmapset-id}#{mode}/{beatmap-id} format',
-    mp: 'Multiplayer match URLs should use the /community/matches/{match-id} format',
-    s: 'Beatmapset URLs should use the /beatmapsets/{beatmapset-id} format',
-    u: 'User URLs should use the /users/{user-id} format',
-};
+var urlRegex = /^https?:\/\/(?:osu|new)\.ppy\.sh\/(.+?)\/?$/;
+var uriWarnings = [
+    [/^b(?:eatmaps)?\/\d+$/, 'Beatmap URLs should use the /beatmapsets/{beatmapset-id}#{mode}/{beatmap-id} format'],
+    [/^mp\/\d+$/, 'Multiplayer match URLs should use the /community/matches/{match-id} format'],
+    [/^s\/\d+$/, 'Beatmapset URLs should use the /beatmapsets/{beatmapset-id} format'],
+    [/^u\/\d+$/, 'User URLs should use the /users/{user-id} format'],
+];
 
 function osuLinks(tree, file) {
     visit(tree, 'link', visitor);
@@ -20,14 +19,13 @@ function osuLinks(tree, file) {
         if (generated(node))
             return;
 
-        var uriMatch = node.url.match(uriRegex);
+        var uriMatch = node.url.match(urlRegex);
 
-        if (uriMatch !== null) {
-            var warning = uriWarnings[uriMatch[1]];
-
-            if (warning !== undefined)
-                file.message(warning, node);
-        }
+        if (uriMatch !== null)
+            uriWarnings.forEach(function ([uriRegex, warning]) {
+                if (uriRegex.test(uriMatch[1]))
+                    file.message(warning, node);
+            });
     }
 }
 
